c29ba5515ab444d73ca1c4efd38c04846f754099
Ensar Sarac <76907308+ensarsarac@users.noreply.github.com>
2024-07-27 23:16:49 +0300

    Add project files.

---
+++ Infrastructer/Aranoz.Persistance/Repositories/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Aranoz.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aranoz.Persistance.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+    }
+}
