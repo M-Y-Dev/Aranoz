6f05207c8da3666b6a6f64d230fe233cb47145ae
Yahyaygmr <yahyaygmr@gmail.com>
2024-07-30 10:48:02 +0300

    comment and message entites were created
    comment and message entites were created

---
+++ Core/Aranoz.Domain/Entity/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aranoz.Domain.Entity
+{
+    public class Comment
+    {        
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int Rating { get; set; }
+        public string CommentContent { get; set; }
+        public bool Status { get; set; }
+    }
+}
