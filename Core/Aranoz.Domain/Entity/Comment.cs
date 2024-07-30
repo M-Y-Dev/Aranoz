48d3914e46d8280b521eaca2daa90c75cdf22bcb
Yahyaygmr <yahyaygmr@gmail.com>
2024-07-30 15:32:52 +0300

    comment fixed
    comment entitys inheritance deficiency has been resolved

--- Core/Aranoz.Domain/Entity/Comment.cs
+++ Core/Aranoz.Domain/Entity/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aranoz.Domain.Entity.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace Aranoz.Domain.Entity
 {
-    public class Comment
-    {        
+    public class Comment : BaseEntity
+    {
         public string FullName { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
