2efe3700f2b889a36d940a8d529a73f8da136b3b
MnrMine <mnr_mine_agri@hotmail.com>
2024-07-31 11:40:36 +0300

    Products and Address entities have been created

---
+++ Core/Aranoz.Domain/Entity/Product.cs
@@ -0,0 +1,21 @@
+﻿using Aranoz.Domain.Entity.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aranoz.Domain.Entity
+{
+    public class Product:BaseEntity
+    {
+        public string ProductName { get; set; }
+        public string ProductImageUrl { get; set; }
+        public int ProductStock { get; set; }
+        public decimal Price { get; set; }
+        //iliskilendirme yaptım eklenince dahil edilecek.
+        //public int CategoryID { get; set; }
+        //public Category Category { get; set; }
+        //public List<ProductDetail> ProductDetails { get; set; }
+    }
+}
