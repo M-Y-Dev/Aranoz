7bcafa61cf8d8ac9928dce121eaaebc08103f7bd
Batuhan Zanlıer <batuhanzanlier@outlook.com>
2024-07-31 11:08:47 +0300

    Order, AppUser, and AppRole tables have been added.

---
+++ Core/Aranoz.Domain/Entity/Common/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aranoz.Domain.Entity.Common
+{
+    public class AppUser
+    {
+    }
+}
---
+++ Core/Aranoz.Domain/Entity/Common/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aranoz.Domain.Entity.Common
+{
+    public class Order
+    { 
+        public int OrderId { get; set; } 
+        public int AppUserId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal UnitPrice { get; set; } // Ürünün adet fiyatı
+        public DateTime OrderDate { get; set; } 
+
+    }
+}
