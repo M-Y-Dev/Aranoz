95c97d838f72844cca45ef05296df561135d4c8f
fthatmc <136472585+fthatmc@users.noreply.github.com>
2024-07-31 16:14:30 +0300

    Created category and orderdetail entities.

---
+++ Core/Aranoz.Domain/Entity/Category.cs
@@ -0,0 +1,15 @@
+﻿using Aranoz.Domain.Entity.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aranoz.Domain.Entity
+{
+    public class Category : BaseEntity
+    {
+        public string CategoryName { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
