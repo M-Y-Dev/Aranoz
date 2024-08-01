df86ba4ada34b433f51467531d1ffaea16f1abf7
MnrMine <mnr_mine_agri@hotmail.com>
2024-08-01 11:34:35 +0300

    __the generic repository has been written.

--- Infrastructer/Aranoz.Persistance/Repositories/Repository.cs
+++ Infrastructer/Aranoz.Persistance/Repositories/Repository.cs
@@ -1,4 +1,6 @@
 ﻿using Aranoz.Application.Interfaces;
+using Aranoz.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,40 @@ namespace Aranoz.Persistance.Repositories
 {
     public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly AranozContext  _context;
+
+        public Repository(AranozContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var values = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(values);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
     }
 }
