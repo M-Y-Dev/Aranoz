9a57bc3d0e9fddca6cbf6e8e200019eabf3b2b06
MnrMine <mnr_mine_agri@hotmail.com>
2024-08-05 09:11:11 +0300

    Map Profile updated

--- Core/Aranoz.Application/Mapping/MapProfile.cs
+++ Core/Aranoz.Application/Mapping/MapProfile.cs
@@ -1,22 +1,21 @@
-﻿using Aranoz.Application.Mediator.Commands.ProductCommands;
+﻿using Aranoz.Application.Mediator.Commands.AddressCommands;
+using Aranoz.Application.Mediator.Commands.BasketCommands;
+using Aranoz.Application.Mediator.Commands.CategoryCommands;
+using Aranoz.Application.Mediator.Commands.CommentCommands;
+using Aranoz.Application.Mediator.Commands.ContactCommands;
+using Aranoz.Application.Mediator.Commands.MessageCommands;
+using Aranoz.Application.Mediator.Commands.OrderCommands;
+using Aranoz.Application.Mediator.Commands.ProductCommands;
+using Aranoz.Application.Mediator.Results.AddressResults;
+using Aranoz.Application.Mediator.Results.BasketResults;
+using Aranoz.Application.Mediator.Results.CategoryResults;
+using Aranoz.Application.Mediator.Results.CommentResults;
+using Aranoz.Application.Mediator.Results.ContactResults;
+using Aranoz.Application.Mediator.Results.MessageResults;
+using Aranoz.Application.Mediator.Results.OrderResults;
 using Aranoz.Application.Mediator.Results.ProductResults;
 using Aranoz.Domain.Entity;
 using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Aranoz.Application.Mediator.Commands.ProductCommands;
-using Aranoz.Application.Mediator.Results.ProductResults;
-using Aranoz.Application.Mediator.Commands.ContactCommands;
-using Aranoz.Application.Mediator.Results.ContactResults;
-using Aranoz.Application.Mediator.Commands.OrderCommands;
-using Aranoz.Application.Mediator.Results.OrderResults;
-using Aranoz.Application.Mediator.Commands.CategoryCommands;
-using Aranoz.Application.Mediator.Results.CategoryResults;
-using Aranoz.Application.Mediator.Commands.BasketCommands;
-using Aranoz.Application.Mediator.Results.BasketResults;
 
 namespace Aranoz.Application.Mapping
 {
@@ -33,6 +32,36 @@ namespace Aranoz.Application.Mapping
             CreateMap<Address, UpdateAddressCommand>().ReverseMap();
             CreateMap<Address, GetAddressByIdQueryResult>().ReverseMap();
             CreateMap<Address, GetAddressQueryResult>().ReverseMap();
+
+            CreateMap<Message, CreateMessageCommand>().ReverseMap();
+            CreateMap<Message, UpdateMessageCommand>().ReverseMap();
+            CreateMap<Message, GetMessageByIdQueryResult>().ReverseMap();
+            CreateMap<Message, GetMessageQueryResult>().ReverseMap();
+
+            CreateMap<Comment, CreateCommentCommand>().ReverseMap();
+            CreateMap<Comment, UpdateCommentCommand>().ReverseMap();
+            CreateMap<Comment, GetCommentByIdQueryResult>().ReverseMap();
+            CreateMap<Comment, GetCommentQueryResult>().ReverseMap();
+
+            CreateMap<Contact, CreateContactCommand>().ReverseMap();
+            CreateMap<Contact, UpdateContactCommand>().ReverseMap();
+            CreateMap<Contact, GetContactByIdQueryResult>().ReverseMap();
+            CreateMap<Contact, GetContactQueryResult>().ReverseMap();
+
+            CreateMap<Order, CreateOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+            CreateMap<Order, GetOrderQueryResult>().ReverseMap();
+            CreateMap<Order, GetOrderByIdQueryResult>().ReverseMap();
+
+            CreateMap<Category, CreateCategoryCommand>().ReverseMap();
+            CreateMap<Category, UpdateCategoryCommand>().ReverseMap();
+            CreateMap<Category, GetCategoryByIdQueryResult>().ReverseMap();
+            CreateMap<Category, GetCategoryQueryResult>().ReverseMap();
+
+            CreateMap<Basket, CreateBasketCommand>().ReverseMap();
+            CreateMap<Basket, UpdateBasketCommand>().ReverseMap();
+            CreateMap<Basket, GetBasketByIdQueryResult>().ReverseMap();
+            CreateMap<Basket, GetBasketQueryResult>().ReverseMap();
         }
     }
 }
