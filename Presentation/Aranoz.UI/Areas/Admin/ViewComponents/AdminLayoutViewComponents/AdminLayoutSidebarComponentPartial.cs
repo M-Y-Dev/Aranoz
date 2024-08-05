0c92be3c07c55df7a5ada20395bb331a81e71603
Yahyaygmr <yahyaygmr@gmail.com>
2024-08-05 16:05:19 +0300

    Admin area and layout components were created

---
+++ Presentation/Aranoz.UI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aranoz.UI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class AdminLayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
