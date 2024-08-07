ea7733e0ef6ba9e105c30449b6479c5b1914aed7
Batuhan Zanlıer <batuhanzanlier@outlook.com>
2024-08-07 11:10:41 +0300

    deneme

---
+++ Presentation/Aranoz.UI/ViewComponents/_UIAranozLayout/_UIAranozHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aranoz.UI.ViewComponents._UIAranozLayout
+{
+    public class _UIAranozHeadComponentPartial:ViewComponent
+    { 
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
